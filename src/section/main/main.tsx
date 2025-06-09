e22602a4fc8f21e76cc3c88e60f14c7584200d7b
Shamsiddin007 <shamsiddinruziboyev13@gmail.com>
2025-06-09 22:52:35 +0500

    responsive main

--- src/section/main/main.tsx
+++ src/section/main/main.tsx
@@ -1,19 +1,19 @@
-import Hero from "@/components/hero";
-import MedicalLegal from "@/components/medicalLegal";
-import MainService from "@/components/MainServes";
-import Comments from "@/components/comments";
-// import NewsPage from "@/components/news";
-import InstallPage from "@/components/installPage";
-import QuestionsPage from "@/components/questionsPage";
+import Hero from "@/Components/Hero";
+import MedicalLegal from "@/Components/MedicalLegal";
+import MainService from "@/Components/MainServes";
+import Comments from "@/Components/Comments";
+import NewsPage from "@/Components/News";
+import InstallPage from "@/Components/InstallPage";
+import QuestionsPage from "@/Components/QuestionsPage";
 
 export default function Main() {
   return (
     <div>
         <Hero/>
         <MedicalLegal/>
-        <MainService/>
+        {/* <MainService/> */}
         <Comments/>
-        {/* <NewsPage/> */}
+        <NewsPage/>
         <InstallPage/>
         <QuestionsPage/>
     </div>
