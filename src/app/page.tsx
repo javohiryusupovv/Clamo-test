8ab1843e1c7ed4f57687da04a561798eacec5579
Your Name <you@example.com>
2025-06-10 13:02:15 +0500

    responsive cheked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-// import MainPage from '@/section/Main/Main';
-import RegulatoryDocuments from "@/app/regulatorydocuments/page"
+import MainPage from '@/section/Main/Main';
+// import RegulatoryDocuments from "@/app/regulatorydocuments/page"
 
 
 export default function Main() {
   return (
     <>
-      {/* <MainPage/> */}
-      <RegulatoryDocuments/>
+      <MainPage/>
+      {/* <RegulatoryDocuments/> */}
     </>
   )
 }
