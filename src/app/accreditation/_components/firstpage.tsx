7c44ad3a7dab7e7ef5675cb7433984f12294a08a
kamron005 <kdostov16@gmail.com>
2025-06-11 19:55:20 +0500

    asilbek-yangiladi

--- src/app/accreditation/_components/firstpage.tsx
+++ src/app/accreditation/_components/firstpage.tsx
@@ -4,7 +4,7 @@ import { ChevronRight } from "lucide-react";
 import { Inter } from "next/font/google";
 import Flag from "../../../../public/Images/Flag.png";
 import Flag1 from "../../../../public/Images/Flag2.png";
-
+import "./style.css";
 const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
@@ -17,7 +17,7 @@ function FirstPage() {
     >
       {/* Left Content */}
       <div className="z-10 flex-1">
-        <div className="max-w-full lg:max-w-[557px] relative z-10 flex flex-col gap-[16px] pt-[40px] sm:pt-[50px] md:pt-[60px] lg:pt-[176px] px-1">
+        <div className="max-w-full lg:max-w-[557px] relative z-10 flex flex-col gap-[16px] pt-[25px] sm:pt-[35px] lg:pt-[176px] px-1">
           <h2 className="bg-[#23B3FC] border-0 rounded-[6px] py-1 px-2 leading-[100%] w-fit text-[#FFFFFF] text-[14px]">
             AKKREDITSIYALASH
           </h2>
@@ -66,7 +66,8 @@ function FirstPage() {
       </div>
 
       {/* Bottom Gradient */}
-      <div className="absolute bottom-0 left-0 w-full h-[120px] md:h-[180px] lg:h-[440px] bg-gradient-to-t from-white/100 to-transparent"></div>
+      <div className="absolute bottom-0 left-0 w-full h-[120px] md:h-[180px] lg:h-[440px] bg-gradient-to-t from-[#F6F9FC]/100 opacity- to-transparent"></div>
+      {/* <div className="relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[120px] md:after:h-[180px] lg:after:h-[440px] after:bg-gradient-to-t after:from-white after:to-transparent after:z-10 after:pointer-events-none"></div> */}
     </div>
   );
 }
