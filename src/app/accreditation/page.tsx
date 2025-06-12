7ee787011e4ccebf2d7c65c58aba5c163f53b41d
Shamsiddin007 <shamsiddinruziboyev13@gmail.com>
2025-06-13 00:17:54 +0500

    katta uzgarish yuq

--- src/app/accreditation/page.tsx
+++ src/app/accreditation/page.tsx
@@ -1,12 +1,12 @@
 import Firstpage from "./_components/firstpage"
 import Secondpage from "./_components/secondpage"
-import Question from "./_components/questions"
+import QuestionsPage from "@/Components/QuestionsPage"
 export default function page() {
   return (
     <div>
       <Firstpage/>
       <Secondpage/>
-      <Question/>
+      <QuestionsPage/>
       </div>
   )
 }
