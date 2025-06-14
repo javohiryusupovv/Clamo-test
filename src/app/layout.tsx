37054360c75d8b1c75fa5551724f76d68c7e5d65
Odilhon13 <yusupovjavoxir11@gmail.com>
2025-06-14 09:26:41 +0500

    fix news3 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import Navbar from "@/app/components/Navbar";
-import FooterLayout from "./components/Footer/Footer";
+import FooterLayout from "@/app/components/footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +31,7 @@ export default function RootLayout({
         <Navbar/>
         <ToastContainer />
         {children}
-        <FooterLayout />
+        <FooterLayout/>
       </body>
     </html>
   );
