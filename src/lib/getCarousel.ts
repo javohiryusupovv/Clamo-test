7bc8a1f22127b5e10a444b6b4fb8d32a38e28b8b
Shamsiddin007 <shamsiddinruziboyev13@gmail.com>
2025-06-17 14:15:37 +0500

    lib folder creted

---
+++ src/lib/getCarousel.ts
@@ -0,0 +1,14 @@
+// src/lib/getCarousel.ts
+export async function getCarousel() {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/about/directions/`,
+      {
+        next: { revalidate: 60 },
+        cache: "no-store",
+      }
+    );
+
+    if (!res.ok) throw new Error("Yangiliklarni olishda xatolik");
+
+    return res.json();
+  }
