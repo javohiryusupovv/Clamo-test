cb17704a0f9ae4924616bc8737f8ee316628f871
Shamsiddin007 <shamsiddinruziboyev13@gmail.com>
2025-06-17 14:04:30 +0500

    messege

--- src/app/[locale]/(root)/consulting/page.tsx
+++ src/app/[locale]/(root)/consulting/page.tsx
@@ -1,12 +1,25 @@
 import Firstpage from "./_components/firstpage";
-import Secondpage from "./_components/secondpage"
-function page() {
+import Secondpage from "./_components/secondpage";
+
+export async function getCarousel() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/about/directions/`,
+    {
+      next: { revalidate: 60 },
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) throw new Error("Yangiliklarni olishda xatolik");
+
+  return res.json();
+}
+export default async function Page() {
+  const carouselData = await getCarousel();
   return (
     <div>
       <Firstpage />
-      <Secondpage/>
+      <Secondpage carousel={carouselData} />
     </div>
   );
 }
-
-export default page;
