2a1a3d4506ad87874578c8acf84c913c23c5d381
Odilhon13 <yusupovjavoxir11@gmail.com>
2025-06-24 18:40:17 +0500

    fix 2 3 2 34

--- src/app/[locale]/(root)/license/_components/ReyesterClinet.tsx
+++ src/app/[locale]/(root)/license/_components/ReyesterClinet.tsx
@@ -55,7 +55,7 @@ export default function ReyesterClient({
 
         <div className="relative">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
-          <input
+            <input
             type="search"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
--- src/app/[locale]/(root)/news/_components/NewsGrid.tsx
+++ src/app/[locale]/(root)/news/_components/NewsGrid.tsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { NewsTypes } from "../../../../../../app.types";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { getLocalizedValue, pickStringProps } from "@/lib/getLocalization";
 
-
 export default function NewsGrid({ news }: { news: NewsTypes[] }) {
-  const [search, setSearch] = useState<string | undefined>();
+  const [search, setSearch] = useState<string>("");
+  const t = useTranslations("HomePage");
   const locale = useLocale();
 
   return (
     <div className="container mx-auto px-4 py-8">
       {/* Header and Search */}
       <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-6 gap-4">
-        <h1 className="text-2xl sm:text-3xl font-bold">Yangiliklar</h1>
+        <h1 className="text-2xl sm:text-3xl font-bold">{t("news_title")}</h1>
         <div className="w-full sm:w-1/3">
           <div className="relative">
             <input
@@ -33,45 +33,59 @@ export default function NewsGrid({ news }: { news: NewsTypes[] }) {
 
       {/* Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {news.map((item) => {
-          const stringItem = pickStringProps(item);
-          const localeTitle = getLocalizedValue(stringItem, "title", locale);
-          const localeDescription = getLocalizedValue(
-            stringItem,
-            "description",
-            locale
-          );
-
-          return (
-            <Link key={item.id} href={`/${locale}/news/${item.slug}`}>
-              <div className="bg-white rounded-2xl border border-white hover:border-blue-400 hover:shadow-md transition duration-300 cursor-pointer">
-                <div className="m-3">
-                  <Image
-                    src={item.image}
-                    alt={localeTitle}
-                    className="w-full h-40 object-cover rounded-2xl"
-                    width={349}
-                    height={196}
-                    priority={false}
-                  />
-                </div>
-                <div className="p-4 space-y-2">
-                  <div className="text-sm text-gray-500">
-                    <p className="text-gray-600">
+        {news
+          .filter((item) => {
+            const stringItem = pickStringProps(item);
+            const title = getLocalizedValue(
+              stringItem,
+              "title",
+              locale
+            ).toLowerCase();
+            const description = getLocalizedValue(
+              stringItem,
+              "description",
+              locale
+            ).toLowerCase();
+            return (
+              title.includes(search.toLowerCase()) ||
+              description.includes(search.toLowerCase())
+            );
+          })
+          .map((item) => {
+            const stringItem = pickStringProps(item);
+            const localeTitle = getLocalizedValue(stringItem, "title", locale);
+            const localeDescription = getLocalizedValue(
+              stringItem,
+              "description",
+              locale
+            );
+            return (
+              <Link key={item.id} href={`/${locale}/news/${item.slug}`}>
+                <div className="bg-white rounded-2xl border border-white hover:border-blue-400 hover:shadow-md transition duration-300 cursor-pointer">
+                  <div className="m-3">
+                    <Image
+                      src={item.image}
+                      alt={localeTitle}
+                      className="w-full h-40 object-cover rounded-2xl"
+                      width={349}
+                      height={196}
+                    />
+                  </div>
+                  <div className="p-4 space-y-2">
+                    <p className="text-sm text-gray-500">
                       {item.created_at.slice(0, 10)}
                     </p>
+                    <h2 className="text-lg font-semibold leading-snug line-clamp-2">
+                      {localeTitle}
+                    </h2>
+                    <p className="text-sm text-gray-600 line-clamp-2">
+                      {localeDescription}
+                    </p>
                   </div>
-                  <h2 className="text-lg font-semibold leading-snug line-clamp-2">
-                    {localeTitle}
-                  </h2>
-                  <p className="text-sm text-gray-600 line-clamp-2">
-                    {localeDescription}
-                  </p>
                 </div>
-              </div>
-            </Link>
-          );
-        })}
+              </Link>
+            );
+          })}
       </div>
     </div>
   );
