bf67a5494c02d7bd000ce24049ad00e9e5370074
Yusupov Javohir <105767169+Javohir003@users.noreply.github.com>
2025-06-30 08:11:05 +0500

    Update Hero.tsx

--- src/app/[locale]/(root)/components/Hero.tsx
+++ src/app/[locale]/(root)/components/Hero.tsx
@@ -65,6 +65,7 @@ export default function Hero() {
                   <FaChevronRight className="group-hover:translate-x-1 transition-all duration-200" />
                 </button>
               </Link>
+              </article>
           </div>
         </div>
         <div className="flex flex-col justify-end items-center w-auto relative -bottom-28 max-ms:-bottom-20 max-lg:bottom-[20px] right-0">
