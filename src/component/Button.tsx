d039aa9bcda0d1ab6281f0057695d94993d1eb68
Javohir <yusupovjavoxir11@gmail.com>
2025-07-01 11:55:59 +0500

    fix SEO metadata 7

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,6 +1,6 @@
 import { FaAngleRight } from "react-icons/fa6";
 
-export default function Button({title }: {title: string}) {
+export default function Button({ title }: { title: string }) {
   return (
     <div>
       <button className="group font-inter lg:w-[183px] w-full h-[40px] flex items-center justify-center gap-[11.5px] hover:bg-[#23B3FC]/[80%] bg-[#23B3FC] transition-all duration-200 rounded-lg text-sm font-medium text-white leading-[130%] ">
@@ -8,18 +8,5 @@ export default function Button({title }: {title: string}) {
         <FaAngleRight className="group-hover:translate-x-1 transition-all duration-200" />
       </button>
     </div>
-  )
-}
-
-
-export default function ButtonLinear({title }: {title: string}) {
-    return (
-      <div>
-        <button className="group font-inter lg:w-[183px] w-full h-[40px] flex items-center justify-center gap-[11.5px] hover:bg-[#23B3FC]/[80%] bg-[#23B3FC] transition-all duration-200 rounded-lg text-sm font-medium text-white leading-[130%] ">
-          {title}
-          <FaAngleRight className="group-hover:translate-x-1 transition-all duration-200" />
-        </button>
-      </div>
-    )
-    t("applying")
+  );
   }
