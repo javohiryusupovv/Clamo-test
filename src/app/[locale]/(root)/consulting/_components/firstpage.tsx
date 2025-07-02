74985e1d427d92f70aa35cbc9b1cc7823cfec5b5
Javohir <yusupovjavoxir11@gmail.com>
2025-07-02 23:51:18 +0500

    fix text fix 22

--- src/app/[locale]/(root)/consulting/_components/firstpage.tsx
+++ src/app/[locale]/(root)/consulting/_components/firstpage.tsx
@@ -5,12 +5,6 @@ import { Inter } from "next/font/google";
 import Man from "../../../../../../public/Images/man.png";
 import Woman from "../../../../../../public/Images/woman.png";
 import Image from "next/image";
-// import "./style.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-});
 
 export default function firstpage() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
