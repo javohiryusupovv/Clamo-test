cbaddd5b14e6921e9447b8bb595bb0d32bdc5458
Javohir <yusupovjavoxir11@gmail.com>
2025-07-03 00:29:03 +0500

    fix text fix nugo75u

--- src/app/[locale]/(root)/cabinet-clamo/page.tsx
+++ src/app/[locale]/(root)/cabinet-clamo/page.tsx
@@ -7,8 +7,8 @@ export default function Page() {
   const t = useTranslations("ComingSoon");
 
   return (
-    <div className="sm:h-screen h-[65vh] flex items-center justify-center flex-col gap-7 text-center">
-      <h4 className="text-[25px] font-semibold">
+    <div className="sm:h-screen h-[75vh] flex items-center justify-center flex-col gap-7 text-center">
+      <h4 className="text-[25px] font-semibold leading-[130%]">
         ⏳ {t("message")}
       </h4>
       <Link
