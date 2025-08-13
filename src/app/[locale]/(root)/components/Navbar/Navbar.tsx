3a8bd8365c944bd4600a9c234fb17b7f27976d81
Yusupov Javohir <105767169+Javohir003@users.noreply.github.com>
2025-08-13 23:17:49 +0500

    Update Navbar.tsx

--- src/app/[locale]/(root)/components/Navbar/Navbar.tsx
+++ src/app/[locale]/(root)/components/Navbar/Navbar.tsx
@@ -214,7 +214,7 @@ export default function Navbar() {
                 </Link>
               </li>
               <li>
-                <Link href={`/${locale}/license`} aria-label="License Page" onClick={() => setIsMobileMenuOpen(false)} className="text-[15px] font-medium sm:text-[17px] sm:font-semibold text-[#3D445E] hover:text-[#23B3FC] transition-all duration-200">
+                <Link href={`/${locale}/licensing`} aria-label="License Page" onClick={() => setIsMobileMenuOpen(false)} className="text-[15px] font-medium sm:text-[17px] sm:font-semibold text-[#3D445E] hover:text-[#23B3FC] transition-all duration-200">
                   {t("licens")}
                 </Link>
               </li>
